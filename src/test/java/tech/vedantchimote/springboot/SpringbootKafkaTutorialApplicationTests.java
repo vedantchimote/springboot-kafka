9b4738e58c9b48324847702c6a18390a59a0cc8f
Asta <78469700+astacode@users.noreply.github.com>
2022-12-03 11:13:38 +0530

    commit ready

---
+++ src/test/java/tech/vedantchimote/springboot/SpringbootKafkaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.vedantchimote.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
